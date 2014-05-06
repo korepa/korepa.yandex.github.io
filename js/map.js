10aca5a01b318652fc5ffd09e5cd7e2d1b5133b1
Knyazev Ivan <to.knyazev@gmail.com>
2014-05-07 02:08:45 +0400

    07.05
    Добавил всплывающие подсказки по адресу.
    Поменял иконки по-умолчанию в браузере (вкладка).
    Убрал некоторые лишние элементы

--- js/map.js
+++ js/map.js
@@ -1,12 +1,14 @@
 // Дождёмся загрузки API и готовности DOM.
 ymaps.ready(init);
+
+// инициализация переменных
 var myMap;
+var myCollection;
 
 function init () {
     var myPlacemark;
     myMap = new ymaps.Map('map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
+        // указываем центр и масштаб карты
         center:[55.76, 37.64], // Москва
         zoom:11,
         behaviors: ['default', 'scrollZoom']
@@ -56,25 +58,39 @@ function init () {
         });
     }
 
+    // нажимаем на кнопку "Заказать"
     document.getElementById('orderButton').onclick = function () {
         // используем для заказа доставки
         window.location = 'payment.html'
     };
+
+    // обрабатываем нажатие на кнопку "Enter" (строим маршрут)
+    document.getElementById('fromText').onkeyup = function (event) {
+        if (event.keyCode == 13) {
+            $("#routeButton").click();
+        }
+    };
+    document.getElementById('toText').onkeyup = function (event) {
+        if (event.keyCode == 13) {
+            $("#routeButton").click();
+        }
+    };
 }
 
-var route1;
+// функция построения марштура от точки А к точке В
+function makeRoute() {
 
-function route() {
-
+    //myMap.geoObjects.empty();
     // очистим сперва предущие маршруты
-    //if (route1 != null) myMap.geoObjects.remove(route1);
+    //if (route1 != null)
+    //    myMap.geoObjects.removeAllRanges();
     //route = null;
 
     // откуда и куда
     var fromAddress = document.getElementById('fromText').value;
     var toAddress = document.getElementById('toText').value;
 
-    route1 = [
+    var route1 = [
         fromAddress,
         toAddress
     ];
@@ -85,11 +101,17 @@ function route() {
             mapStateAutoApply: true
         }
         ).then(function (route) {
-            myMap.geoObjects.add(route);
+
+            // удалим предыдущий маршрут, если етсь
+            if (myCollection != null)
+                myMap.geoObjects.remove(myCollection);
+
+            // добавим марштрут
+            myCollection = new ymaps.GeoObjectCollection();
+            myCollection.add(route);
+            myMap.geoObjects.add(myCollection);
 
             // Зададим содержание иконок начальной и конечной точкам маршрута.
-            // С помощью метода getWayPoints() получаем массив точек маршрута.
-            // Массив транзитных точек маршрута можно получить с помощью метода getViaPoints.
             var points = route.getWayPoints(),
                 lastPoint = points.getLength() - 1;
             // Задаем стиль метки - иконки будут красного цвета, и
@@ -99,32 +121,6 @@ function route() {
             points.get(0).properties.set('iconContent', fromAddress);
             points.get(lastPoint).properties.set('iconContent', toAddress);
 
-            // Проанализируем маршрут по сегментам.
-            // Сегмент - участок маршрута, который нужно проехать до следующего
-            // изменения направления движения.
-            // Для того, чтобы получить сегменты маршрута, сначала необходимо получить
-            // отдельно каждый путь маршрута.
-            // Весь маршрут делится на два пути:
-            // 1) от улицы Крылатские холмы до станции "Кунцевская";
-            // 2) от станции "Кунцевская" до "Пионерская".
-
-            var moveList = 'Трогаемся,</br>',
-                way,
-                segments;
-            // Получаем массив путей.
-            for (var i = 0; i < route.getPaths().getLength(); i++) {
-                way = route.getPaths().get(i);
-                segments = way.getSegments();
-                for (var j = 0; j < segments.length; j++) {
-                    var street = segments[j].getStreet();
-                    moveList += ('Едем ' + segments[j].getHumanAction() + (street ? ' на ' + street : '') + ', проезжаем ' + segments[j].getLength() + ' м.,');
-                    moveList += '</br>'
-                }
-            }
-            moveList += 'Останавливаемся.';
-            // Выводим маршрутный лист.
-            $('#list').empty();
-            $('#list').append(moveList);
         }, function (error) {
             alert('Возникла ошибка: ' + error.message);
         });
---
+++ js/search.js
@@ -0,0 +1,166 @@
+var suggest_count=0;
+var suggest_selected=0;
+var input_initial_value='';
+var search_suggestion_url="http://suggest-maps.yandex.ru/suggest-geo?callback=show_suggestion&_=1302005670080&lang=ru-RU&ll=37.617671%2C55.755768&spn=2.399139%2C0.624969&highlight=1&fullpath=1&sep=1&search_type=all&part=";
+var this_element;
+
+function reposition_search_suggestion(pa){
+    var rr=getDim(pa);
+    var pos_left = rr.x;
+    var pos_top = (rr.y + pa.offsetHeight);
+    $('#search_suggestion').css({'left':pos_left,'top':Math.ceil(pos_top)});
+}
+
+$(window).load(function(){
+    if(!search_suggestion_url)return;
+    var typingTimer;                //timer identifier
+    var doneTypingInterval = 42;  //time in ms
+
+    if(document.getElementById('search_suggestion')===null){
+        $('<ul id="search_suggestion"></ul>').appendTo('body').css({'display':'none','position':'absolute'});
+    }
+    //Если keyup, start the countdown
+    $("#toText").keyup(function(I){
+        switch(I.keyCode) {
+            case 18:case 9: break;
+            case 38:case 40:break;
+            default: typingTimer = setTimeout(doneTyping, doneTypingInterval);
+                break;
+        }
+    });
+
+    $('html').click(function(){
+        $('#search_suggestion').hide();
+    });
+
+    $('#search_suggestion').click(function(event){
+        if(suggest_count)
+            $('#search_suggestion').show();
+        event.stopPropagation();
+    });
+
+    $("#toText").focusin(function(){
+        this_element=this;
+        reposition_search_suggestion(this);
+        if(suggest_count)
+            $('#search_suggestion').show();
+    });
+
+
+    $("#toText").click(function(event){
+        this_element=this;
+        if(suggest_count)
+            $('#search_suggestion').show();
+        if(suggest_selected>0){
+            $('#search_suggestion li').eq(suggest_selected-1).removeClass('active');
+            suggest_selected=0;
+            $(this).attr('value',input_initial_value);
+        }
+        event.stopPropagation();
+    });
+    $("#toText").keypress(function(J) {
+        switch(J.keyCode) {
+            case 13:
+                if(suggest_selected===0)
+                    $(this).closest("form").submit();
+                else {
+                    confirm_search();
+                    J.preventDefault();
+                } //window.location=$('#search_suggestion li a').eq(suggest_selected-1).attr('href');
+                return false;
+            case 27:
+                return false;
+        }
+    });
+
+    //on keydown, clear the countdown
+    $("#toText").keydown(function(I){
+        this_element=this;
+        reposition_search_suggestion(this);
+        switch(I.keyCode) {
+            case 18:case 9: $('#search_suggestion').hide();break;
+            case 38:case 40:
+            I.preventDefault();
+            if(suggest_count)
+                key_activate(I.keyCode-39)
+            break;
+            case 13:break;
+            case 27:
+                $(this).click();
+                break;
+            default:
+                clearTimeout(typingTimer);
+                break;
+        }
+    });
+
+    function doneTyping (){
+        $("body").append($('<script charset="utf-8" src="'+search_suggestion_url+escape((encode_utf8($(this_element).attr("value"))))+'">'));
+    }
+});
+
+function confirm_search(value){
+    $('#search_suggestion').hide();
+    if(value)
+        $(this_element).attr('value',value);
+    suggest_selected=0;
+    suggest_count=0;
+}
+
+function getDim(el){
+    for (var lx=0,ly=0;el!=null;
+         lx+=el.offsetLeft,ly+=el.offsetTop,el=el.offsetParent);
+    return {x:lx,y:ly};
+}
+
+function chr(code){
+    return String.fromCharCode(code);
+}
+
+// UTF-8 encode / decode by Johan Sundstr?m
+function encode_utf8(s){
+    return unescape(encodeURIComponent( s ));
+}
+
+function show_suggestion(res){
+    if(res[1] && res[1].length){
+        input_initial_value=res[0];
+        suggest_count=res[1].length;
+        suggest_selected=0;
+        $('#search_suggestion li').each(function(){$(this).remove()});
+        for(var i in res[1]){
+            if(typeof res[1][i][2]==='undefined')
+                continue;
+            var option = document.createElement("li")
+            option.innerHTML='<a href="#" onclick="confirm_search(this.innerHTML);return false;">'+res[1][i][2]+'</a>';
+            document.getElementById('search_suggestion').appendChild(option);
+        }
+        $('#search_suggestion').show(500);
+    }
+    else {
+        suggest_count=0;
+        $('#search_suggestion').hide();
+    }
+}
+
+function key_activate(n){
+    $('#search_suggestion li').eq(suggest_selected-1).removeClass('active');
+    if(n===1 && suggest_selected<suggest_count){
+        suggest_selected++;
+    }
+    if(n===-1 && suggest_selected>0){
+        suggest_selected--;
+    }
+    if(suggest_selected>0){
+        $('#search_suggestion li').eq(suggest_selected-1).addClass('active');
+        $(this_element).attr("value",$('#search_suggestion li a').eq(suggest_selected-1).text());
+        $(item_price_element).attr("value",$('#search_suggestion li b').eq(suggest_selected-1).text());
+    }
+    else {
+        $(this_element).attr("value",input_initial_value);
+    }
+}
+
+function unhtml(s){
+    return s.toString().replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&amp;/g, '&').replace(/&quot;/g, '"');
+}
