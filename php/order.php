56c180b7b820ee65d4430869c67f13934b832d34
Knyazev Ivan <to.knyazev@gmail.com>
2014-08-06 14:40:42 +0400

    06.08_5
    поправили скрипт order.php

--- php/order.php
+++ php/order.php
@@ -2,22 +2,22 @@
     function order()
     {
         // объявим переменные
-        $dir = $_GET['dir']);
-        $to = $_GET['to']);
-        $to2 = $_GET['to2']);
-        $to3 = $_GET['to3']);
-        $from = $_GET['from']);
-        $flightNumber = $_GET['flightnumber']);
-        $date = $_GET['date']);
-        $time = $_GET['time']);
-        $name = $_GET['name']);
-        $phone = $_GET['phone']);
-        $email = $_GET['email']);
-        $passCount = $_GET['passcount']);
-        $childrenCount = $_GET['childrenCount']);
-        $sign = $_GET['sign']);
-        $PayType = $_GET['payType']);
-        $price = $_GET['price']);
+        $dir = $_GET['dir'];
+        $to = $_GET['to'];
+        $to2 = $_GET['to2'];
+        $to3 = $_GET['to3'];
+        $from = $_GET['from'];
+        $flightNumber = $_GET['flightnumber'];
+        $date = $_GET['date'];
+        $time = $_GET['time'];
+        $name = $_GET['name'];
+        $phone = $_GET['phone'];
+        $email = $_GET['email'];
+        $passCount = $_GET['passcount'];
+        $childrenCount = $_GET['childrenCount'];
+        $sign = $_GET['sign'];
+        $payType = $_GET['payType'];
+        $price = $_GET['price'];
 
         // производим действия
         $retval = 0;
